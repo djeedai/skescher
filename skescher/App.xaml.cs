0f7f680b1493aab8ec348bb849d79324e78eba95
Jerome Humbert <djeedai@gmail.com>
2019-08-18 20:52:38 +0100

    Start in fullscreen by default

--- skescher/App.xaml.cs
+++ skescher/App.xaml.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -28,6 +29,9 @@ namespace SketchPad2
         /// </summary>
         public App()
         {
+            // Launch in fullscreen by default
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
